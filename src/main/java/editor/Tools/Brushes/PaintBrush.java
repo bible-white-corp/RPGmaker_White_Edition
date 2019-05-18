c87c5e9ef56754e67a43f583384ccc09d5a432cb
Florian Gabille <florian.gabille@epita.fr>
2019-05-18 16:34:20 +0200

    [Tools] Add all Brushes

---
+++ src/main/java/editor/Tools/Brushes/PaintBrush.java
@@ -0,0 +1,13 @@
+package editor.Tools.Brushes;
+
+import editor.Maps.Level;
+import editor.Tools.Brush;
+import editor.Tools.Selection;
+
+public class PaintBrush extends Brush {
+
+    @Override
+    public void paintSelection(Level level, Selection selection, int x_pixel, int y_pixel) {
+
+    }
+}
---
+++ src/main/java/editor/Tools/Brushes/Pencil.java
@@ -0,0 +1,13 @@
+package editor.Tools.Brushes;
+
+import editor.Maps.Level;
+import editor.Tools.Brush;
+import editor.Tools.Selection;
+
+public class Pencil extends Brush {
+
+    @Override
+    public void paintSelection(Level level, Selection selection, int x_pixel, int y_pixel) {
+
+    }
+}
