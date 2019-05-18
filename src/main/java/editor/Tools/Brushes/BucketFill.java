8a95c74ef19d22ca749632d7e04d2cc6c45e2170
Florian Gabille <florian.gabille@epita.fr>
2019-05-18 17:39:46 +0200

    [Tools] Create FillBucket but need improvement

--- src/main/java/editor/Tools/Brushes/BucketFill.java
+++ src/main/java/editor/Tools/Brushes/BucketFill.java
@@ -1,13 +1,39 @@
 package editor.Tools.Brushes;
 
 import editor.Maps.Level;
+import editor.Tiles.Tile;
 import editor.Tools.Brush;
 import editor.Tools.Selection;
 
 public class BucketFill extends Brush {
+
+    private Tile background;
+
     @Override
     public void clicked(Level level, Selection selection, int x_pixel, int y_pixel) {
 
+        background = level.getTilePixel(x_pixel, y_pixel);
+
+        fill(level, selection, x_pixel, y_pixel);
+    }
+
+    private void fill(Level level, Selection selection, int x_pixel, int y_pixel)
+    {
+        try {
+
+            if (level.getTilePixel(x_pixel, y_pixel) == background) {
+                level.setTilePixel(selection.getTiles().get(0), x_pixel, y_pixel);
+
+                fill(level, selection, x_pixel + level.getTileWidth(), y_pixel);
+                fill(level, selection, x_pixel - level.getTileWidth(), y_pixel);
+                fill(level, selection, x_pixel, y_pixel + level.getTileHeight());
+                fill(level, selection, x_pixel, y_pixel - level.getTileHeight());
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
     }
 
     @Override
