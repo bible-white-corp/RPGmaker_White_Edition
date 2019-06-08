840c0ab06de1f22eab7559c61f6e1d5eb3a60c63
louis.holleville <louis.holleville@epita.fr>
2019-06-08 19:11:17 +0200

    [MOVES] Modified random moves to REAL random moves

--- src/main/java/editor/Object/PathFinding/PathSettings.java
+++ src/main/java/editor/Object/PathFinding/PathSettings.java
@@ -2,6 +2,7 @@ package editor.Object.PathFinding;
 
 import editor.Editor;
 import editor.Maps.Level;
+import editor.Object.ObjectInstantiation;
 import engine.Controllers.PlayerControl;
 import engine.Engine;
 
@@ -19,7 +20,7 @@ public class PathSettings {
     int levelIndex;
     int parentObj;
 
-    public PathSettings(MoveType type, Point coord, int levelIndex, int objIndex){
+    public PathSettings(MoveType type, Point coord, int levelIndex, int objIndex) {
         this.type = type;
         paths = new Vector<>();
         paths.add(coord);
@@ -37,60 +38,74 @@ public class PathSettings {
         this.type = type;
     }
 
-    public Point getCur(){
+    public Point getCur() {
         if (type == MoveType.idle)
             return paths.get(0);
         return paths.get(curPath);
     }
 
-    public Point setNext(){
-        if (type == MoveType.simple_trip){
-           type = MoveType.idle;
-        }
-        else if (type == MoveType.two_way){
-            if (++curPath != paths.size()){
+    public Point setNext() {
+        if (type == MoveType.simple_trip) {
+            type = MoveType.idle;
+        } else if (type == MoveType.two_way) {
+            if (++curPath != paths.size()) {
                 return getCur();
             }
             type = MoveType.idle;
-        }
-        else if (type == MoveType.infinite){
-            if (curIt >= 1){
+        } else if (type == MoveType.infinite) {
+            if (curIt >= 1) {
                 if (--curPath < 0) {
                     curIt = 0;
                     curPath += 2;
                 }
                 return getCur();
-            }
-            else {
-                if (++curPath >= paths.size()){
+            } else {
+                if (++curPath >= paths.size()) {
                     curIt = 1;
                     curPath -= 2;
                 }
                 return getCur();
             }
-        }
-        else if (type == MoveType.random){
+        } else if (type == MoveType.random) {
             Level l = Editor.world.levelList.get(levelIndex);
             Point p;
-            int x;
-            int y;
-            do{
-                x = random.nextInt(l.getWidth());
-                y = random.nextInt(l.getHeight());
-                p = new Point(x * l.getTileWidth() - l.getTileWidth()/2,
-                        y * l.getTileHeight() - l.getTileHeight() / 2);
-            } while (!PlayerControl.canMove(p, levelIndex, Engine.world.worldObjects.getInWorldObj().get(parentObj)));
+            ObjectInstantiation parent = Engine.world.worldObjects.getInWorldObj().get(parentObj);
+            Point curPos = parent.getPosition();
+            Level level = Engine.world.levelList.get(Engine.getEngineFrame().getDisplay().getLevel());
+            int x, y, r;
+            do {
+                r = random.nextInt(4);
+                switch (r) {
+                    case 0:
+                        x = 1;
+                        y = 0;
+                        break;
+                    case 1:
+                        x = 0;
+                        y = 1;
+                        break;
+                    case 2:
+                        x = -1;
+                        y = 0;
+                        break;
+                    default:
+                        x = 0;
+                        y = -1;
+                        break;
+                }
+                p = new Point(curPos.x + x * level.getTileWidth(), curPos.y + y * level.getTileHeight());
+            } while (!PlayerControl.canMove(p, levelIndex, parent));
             paths.set(0, p);
             return paths.get(0);
         }
         return null;
     }
 
-    public void addPoint(Point p){
-            paths.set(0, p);
+    public void addPoint(Point p) {
+        paths.set(0, p);
     }
 
-    public void setOrigin(Point p){
+    public void setOrigin(Point p) {
         paths.set(1, p);
     }
 }
